fa031289d5280210c3f939a76d101bf3ae9a6b72
Cianee Sumowalt <sumowaltc@gmail.com>
2023-03-09 20:33:24 -0500

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,63 @@
+public class MergeSort {
+    public static void main(String[] args) {
+      int[] array = {8,2,5,3,4,7,6,1};
+      
+      mergeSort(array);
+      
+      for (int i = 0; i <array.length;i++){
+        System.out.println(array[i] + " ");
+      }
+      }
+      private static void mergeSort(int[] array){
+        int length = array.length;
+        if (length <= 1) return; // base case
+        
+        int mid = length/ 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+        
+        int i = 0; // left ArrayIndexOutOfBoundsException
+        int j = 0; // right ArrayIndexOutOfBoundsException
+        for(; i <length; i++){
+          if (i < mid){
+            leftArray[i] = array[i];
+          } else{
+            rightArray[j] = array[i];
+            j++;
+          }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray,rightArray,array);
+    }
+
+      private static void merge(int[] leftArray, int[] rightArray, int[] array){
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+        int i = 0, l = 0, r = 0; // indices for original, left, and right array
+        
+        // check conditions for merging 
+        while(l < leftSize && r < rightSize){
+          if(leftArray[l] < rightArray[r]){
+            array[i] = leftArray[l];
+            i++;
+            l++;
+          } else{
+            array[i] = rightArray[r];
+            i++;
+            r++;
+          }
+        }
+        while (l < leftSize){
+          array[i] = leftArray[l];
+          i++;
+          l++;
+        }
+        while (r < rightSize){
+          array[i] = rightArray[r];
+          i++;
+          r++;
+        }
+      }
+      
+  }
